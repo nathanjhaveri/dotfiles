9b75aa8afdcb63fcd38ba70570c9a4d61fe20663
Nathan Jhaveri <nathanj@porch.com>
2015-11-02 10:00:55 -0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -sf $HOME/code/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/code/dotfiles/.vim $HOME/.vim
+ln -sf $HOME/code/dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/code/dotfiles/.bash_profile $HOME/.bash_profile
+
