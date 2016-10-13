6e1a64bc5a8fe27410b3a4159cad6186fa1414e3
Nathan Jhaveri <nathanj@porch.com>
2016-10-13 09:15:14 -0700

    Fix double .vim dir bug

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@ mkdir -p ~/.vim/_swap
 mkdir -p ~/.vim/_undo
 mkdir -p ~/.config/nvim/
 
-ln -sf $HOME/code/dotfiles/.vimrc $HOME/.vimrc
+rm $HOME/.vim
 ln -sf $HOME/code/dotfiles/.vim/ $HOME/.vim
+ln -sf $HOME/code/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/code/dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/code/dotfiles/.bash_profile $HOME/.bash_profile
-ln -sf $HOME/code/dotfile/.vimrc $HOME/.config/nvim/init.vim
+ln -sf $HOME/code/dotfiles/.vimrc $HOME/.config/nvim/init.vim
 
