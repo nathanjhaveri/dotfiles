c0416f1b0d0166b823f51af2fcba817692cbc5ff
Nathan Jhaveri <nathanj@porch.com>
2017-05-15 09:40:47 -0700

    Move vimrc under .vim so everything is in one directory
    This also makes it easier to use neovim

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 mkdir -p ~/.vim/_backup
 mkdir -p ~/.vim/_swap
 mkdir -p ~/.vim/_undo
-mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/
 
-rm $HOME/.vim
 ln -sf $HOME/code/dotfiles/.vim/ $HOME/.vim
-ln -sf $HOME/code/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/code/dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/code/dotfiles/.bash_profile $HOME/.bash_profile
-ln -sf $HOME/code/dotfiles/.vimrc $HOME/.config/nvim/init.vim
+ln -sf $HOME/code/dotfiles/.vim/ $HOME/.config/nvim
 
